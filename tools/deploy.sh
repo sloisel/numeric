7befc7f8016ede8fe5979ae68d4ff02fe23dea65
Sébastien Loisel <sloisel.pleasenospam@gmail.com>
2011-09-22 20:00:53 +0100

    fix bug

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 d0=`dirname $0`
 cd $d0
-curl http://127.0.0.1/numeric/ > ../../index.html
-curl http://127.0.0.1/numeric/index.php?link=79cfb087074a163a9a763ebe55a4f55545d33a50f54827474cc7e600a5c1ab35 > ../demo.html
+curl http://$HOSTNAME/numeric/ > ../../index.html
+curl http://$HOSTNAME/numeric/index.php?link=79cfb087074a163a9a763ebe55a4f55545d33a50f54827474cc7e600a5c1ab35 > ../demo.html
 php ./mkdb.php
