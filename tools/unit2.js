3027f152ed33bbc0cbc753633404766093cdf308
Sébastien Loisel <sloisel.pleasenospam@gmail.com>
2012-03-20 21:46:42 +0000

    use less common global variables in unit2.js

--- tools/unit2.js
+++ tools/unit2.js
@@ -14,8 +14,8 @@ if(typeof system !== "undefined") {
 } else if(typeof read !== "undefined") {
     myread = read;
 } else {
-    fs = require('fs');
-    myread = (function(x) { return fs.readFileSync(x,'utf8') });
+    _fs = require('fs');
+    myread = (function(x) { return _fs.readFileSync(x,'utf8') });
     console.log(process.cwd());
 }
 function XMLHttpRequest() {
@@ -23,44 +23,44 @@ function XMLHttpRequest() {
     this.open = (function(get,url) { this.responseText = myread(url); });
     this.send = (function() {});
 }
-if(typeof fs !== "undefined") global.XMLHttpRequest = XMLHttpRequest;
-var foo = myread('./documentation.html');
-var baz = myread('./src/numeric.js');
-var ver = baz.match(/numeric.version[ =]*"([0-9.]*)".*/)[1]
-var bar = foo.match(/<pre>[\s\S]*?(?=<\/pre>)/g).join('\n').replace(/<pre>/g,'').split('\n> ')
-var baz = [];
-var k,k0=0;
-for(k=0;k<bar.length;k++) {
-    var j = bar[k].indexOf('\n');
-    if(j>0) { baz[k0] = [bar[k].substring(0,j),bar[k].substring(j+1)]; k0++; }
+if(typeof _fs !== "undefined") global.XMLHttpRequest = XMLHttpRequest;
+var _foo = myread('./documentation.html');
+var _baz = myread('./src/numeric.js');
+var _ver = _baz.match(/numeric.version[ =]*"([0-9.]*)".*/)[1];
+var _bar = _foo.match(/<pre>[\s\S]*?(?=<\/pre>)/g).join('\n').replace(/<pre>/g,'').split('\n> ');
+_baz = [];
+var _k,k0=0;
+for(_k=0;_k<_bar.length;_k++) {
+    var _j = _bar[_k].indexOf('\n');
+    if(_j>0) { _baz[k0] = [_bar[_k].substring(0,_j),_bar[_k].substring(_j+1)]; k0++; }
 }
 
-var numfile = './lib/numeric-'+ver+'.js';
+var numfile = './lib/numeric-'+_ver+'.js';
 var numeric;
-if(typeof fs === "undefined") load(numfile);
+if(typeof _fs === "undefined") load(numfile);
 else { numeric = require('../'+numfile); }
 if(typeof numeric === "undefined") { throw new Error("Could not load numeric.js"); }
 var unit_pass = 0, unit_fail = 0;
-var a,b,msg;
+var _a,_b,_msg;
 var k1 = 0;
 var workshop = {};
-for(k=0;k<k0;k++) {
+for(_k=0;_k<k0;_k++) {
     k1++;
-    bar = '';
+    _bar = '';
     try {
-        workshop.html = function(x) { bar += x; }
-        foo = numeric.prettyPrint(eval(baz[k][0].replace(/&lt;/g,'<').replace(/&gt;/g,'>')));
-        foo = bar+foo;
-    } catch(e) { foo = e.toString(); }
-    a = foo.replace(/\s/g,'');
-    b = baz[k][1].replace(/\s/g,'');
-    if(a===b) {
-        msg = k1+" PASS "+baz[k][0]+"==>"+a+"==="+b;
+        workshop.html = function(x) { _bar += x; }
+        _foo = numeric.prettyPrint(eval(_baz[_k][0].replace(/&lt;/g,'<').replace(/&gt;/g,'>')));
+        _foo = _bar+_foo;
+    } catch(_e) { _foo = _e.toString(); }
+    _a = _foo.replace(/\s/g,'');
+    _b = _baz[_k][1].replace(/\s/g,'');
+    if(_a===_b) {
+        _msg = k1+" PASS "+_baz[_k][0]+"==>"+_a+"==="+_b;
         unit_pass++;
     } else {
-        msg = k1+" FAIL "+baz[k][0]+"==>"+a+"!=="+b;
+        _msg = k1+" FAIL "+_baz[_k][0]+"==>"+_a+"!=="+_b;
         unit_fail++;
     }
-    console.log(msg);
+    console.log(_msg);
 }
 console.log('unit2: '+k1+' tests, '+unit_pass+' pass and '+unit_fail+' fail.');
