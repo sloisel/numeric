c84424aa7a911d1ccaa88096fc8f734aea675ffb
Androbin <robin.richtsfeld@gmail.com>
2017-06-24 00:56:42 +0200

    Use $(...) instead of legacy `...`
    https://github.com/koalaman/shellcheck/wiki/SC2006

--- tools/deploy/clean.sh
+++ tools/deploy/clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-cd `dirname $0`
+cd $(dirname $0)
 cd ../../..
-numeric=`readlink numeric || echo ""`
-staging=`readlink staging || echo ""`
+numeric=$(readlink numeric || echo "")
+staging=$(readlink staging || echo "")
 for x in v*; do
     if [ "z$x" != "z$numeric" -a "z$x" != "z$staging" ]; then
 	rm -rf $x
